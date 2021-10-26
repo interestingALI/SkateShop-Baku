5927fe6da353b4b71b4d6479253342674c484930
Ali Mammedzade <alimammadzada77@gmail.com>
2021-10-26 23:18:46 +0400

    before removing bulma

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { ContactComponent } from "./Contact/contact.component";
+// import { AboutUsComponent } from "./AboutUs/aboutus.component";
+// import { HomeComponent } from "./Home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: HomeComponent,
+  //   pathMatch: 'full'
+  // },
+  // {
+  //   path: 'Contact',
+  //   component: ContactComponent
+  // },
+  // {
+  //   path: 'AboutUs',
+  //   component: AboutUsComponent
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
