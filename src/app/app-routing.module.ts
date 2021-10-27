52aa6103648b7a45060f27bea6048aaccb7d475c
Ali Mammedzade <alimammadzada77@gmail.com>
2021-10-27 23:09:02 +0400

    pre-alpha 0.011

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { ContactComponent } from "./Contact/contact.component";
-// import { AboutUsComponent } from "./AboutUs/aboutus.component";
-// import { HomeComponent } from "./Home/home.component";
+import { ContactComponent } from "./ContactUs/contactus.component";
+import { AboutUsComponent } from "./AboutUs/aboutus.component";
+import { HomeComponent } from "./Home/home.component";
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   component: HomeComponent,
-  //   pathMatch: 'full'
-  // },
-  // {
-  //   path: 'Contact',
-  //   component: ContactComponent
-  // },
-  // {
-  //   path: 'AboutUs',
-  //   component: AboutUsComponent
-  // }
+  {
+    path: '',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'ContactUs',
+    component: ContactComponent
+  },
+  {
+    path: 'AboutUs',
+    component: AboutUsComponent
+  }
 ];
 
 @NgModule({
