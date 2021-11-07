aa1f048c4ad9346f4355ef707466a2179f4c3e22
Ali Mammedzade <alimammadzada77@gmail.com>
2021-11-07 18:40:53 +0400

    pre-alpha 0.35

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { HomeComponent } from './home/home.component';
 import { CoreModule } from "./core/core.module";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   declarations: [
@@ -16,6 +17,7 @@ import { CoreModule } from "./core/core.module";
     HomeComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule,
     CoreModule
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 
 
@@ -12,6 +13,7 @@ import { RouterModule } from '@angular/router';
     FooterComponent
   ],
   imports: [
+    NgbModule,
     CommonModule,
     RouterModule
   ],
