cb1960978027ae4bdb48b8f675e9122a22675601
Ali Mammedzade <alimammadzada77@gmail.com>
2021-11-15 23:18:06 +0400

    pre-alpha 0.79

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,25 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Product } from '../models/product';
+
+
+const apiUrl = 'https://skateshopbaku-default-rtdb.europe-west1.firebasedatabase.app/products/.json'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  products: Product[] = [
-    new Product(1, 'Product 1', 'A neat description for item! Very nice. Very nice.', 100, 'skate', 'black'),
-    new Product(2, 'Product 2', 'A neat description for item! Very nice. Very nice.', 200, 'skate', 'black'),
-    new Product(3, 'Product 3', 'A neat description for item! Very nice. Very nice.', 50, 'skate', 'black'),
-    new Product(4, 'Product 4', 'A neat description for item! Very nice. Very nice.', 120, 'skate', 'black'),
-    new Product(5, 'Product 5', 'A neat description for item! Very nice. Very nice.', 30, 'skate', 'black'),
-    new Product(6, 'Product 6', 'A neat description for item! Very nice. Very nice.', 0, 'skate', 'black'),
-    new Product(7, 'Product 7', 'A neat description for item! Very nice. Very nice.', 20, 'skate', 'black'),
-    new Product(8, 'Product 8', 'A neat description for item! Very nice. Very nice.', 60, 'skate', 'black'),
-  ]
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
-  getProducts():Product[]{
-    return this.products
+  getProducts():Observable<Product[]>{
+    return this.http.get<Product[]>(apiUrl)
   }
 }
--- src/app/shop-page/products/products.component.ts
+++ src/app/shop-page/products/products.component.ts
@@ -14,7 +14,8 @@ export class ProductsComponent implements OnInit {
   constructor(private productService: ProductService){}
 
   ngOnInit(): void {
-    this.productsList = this.productService.getProducts()
+    this.productService.getProducts().subscribe((products) => {
+      this.productsList = products    })
   }
 
 }
