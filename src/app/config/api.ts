d0db1570ed5452cb400354d5768484b698063797
Ali Mammedzade <alimammadzada77@gmail.com>
2021-11-23 16:59:38 +0400

    pre-alpha 0.982

--- src/app/config/api.ts
+++ src/app/config/api.ts
@@ -2,3 +2,4 @@
 export const baseUrl = 'https://skateshopbaku-default-rtdb.europe-west1.firebasedatabase.app'
 export const productsUrl = baseUrl + '/products/.json'
 export const cartUrl = baseUrl + '/cart/.json'
+export const wishlistUrl = baseUrl + '/wishlist/.json'
