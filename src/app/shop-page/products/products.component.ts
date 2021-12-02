3901a8e2c02c2928afb7b62c236c20f17aac7a35
Ali Mammedzade <alimammadzada77@gmail.com>
2021-12-02 22:51:53 +0400

    Alpha 1.6.2

--- src/app/shop-page/products/products.component.ts
+++ src/app/shop-page/products/products.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
+import {Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-products',
@@ -20,8 +22,31 @@ export class ProductsComponent implements OnInit {
   @Input() to = '';
   
   
-  constructor(private productService: ProductService){}
+  constructor(private productService: ProductService, @Inject(DOCUMENT) private document: Document){}
   showSpinner:boolean = true;
+
+//////////////////////////////  scroll to top  ///////////////////
+  windowScrolled: boolean;
+    @HostListener("window:scroll", [])
+    onWindowScroll() {
+        if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+            this.windowScrolled = true;
+        } 
+        else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+            this.windowScrolled = false;
+        }
+    }
+    scrollToTop() {
+        let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+        if (currentScroll > 0) {
+            window.scrollTo(0, 0);
+        }
+    }
+//////////////////////////////////////////////////////////////////
+
+
+
+
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((products) => {
